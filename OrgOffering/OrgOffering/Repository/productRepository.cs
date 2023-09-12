ab83e45320c8ede0729a14c84491272386dc1213
Jacques741 <jacquesccmoll@gmail.com>
2023-09-12 17:57:55 +0200

    Created working webb app and repositorys

---
+++ OrgOffering/OrgOffering/Repository/productRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrgOffering.Data;
+using OrgOffering.Models;
+using System.Threading.Channels;
+
+namespace OrgOffering.Repository
+{
+    public class ProductRepository
+    {
+        protected readonly Demo323Context _context = new Demo323Context();
+
+        // GET ALL: Products
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+
+        // TO DO: Add ‘Get By Id’
+        // TO DO: Add ‘Create’
+        // TO DO: Add ‘Edit’
+        // TO DO: Add ‘Delete’
+        // TO DO: Add ‘Exists’
+    }
+
+
+}
